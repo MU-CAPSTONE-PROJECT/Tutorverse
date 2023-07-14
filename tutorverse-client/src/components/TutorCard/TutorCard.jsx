3ebe5af9f5034e3954fc842cd910fdcf78214cee
Tanya Chisepo <tanyachisepo@meta.com>
2023-07-14 15:10:02 -0700

    Tutor Card for tutor list view

---
+++ tutorverse-client/src/components/TutorCard/TutorCard.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Avatar from '@mui/material/Avatar';
+import './TutorCard.css'
+import { Link } from "react-router-dom";
+
+
+export default function TutorCard ({tutor}){
+
+    //destructing the tutor properties
+    const { id, firstName, lastName, emailAddress, major, school, imageUrl} = tutor
+
+    return (
+        <div className="tutor-card">
+            <Link to={`/tutor/${id}`}>
+            
+                <div>
+                    <div className="left">
+                        <div className="profile-img" >
+                            <Avatar 
+                                alt="Profile Image" 
+                                src={imageUrl}
+                                sx={{ width: 80, height: 80 }}
+                                />
+                        </div>
+                    </div>
+                    <div className="middle">
+                        <div className="name-label">{firstName}  {lastName}</div>
+                        <div className="major-label">{major}</div>
+                        <div className="about-text">Hello. I am an example tutor. Click on me to see more</div>
+                    </div>
+                    <div className="right">
+                        <div className="rating">⭐️⭐️⭐️⭐️⭐️ 5.0</div>
+                        <div className="status-flag">Available </div>
+                    </div> 
+                </div>
+            </Link>
+            
+           
+
+
+        </div>
+    )
+}
