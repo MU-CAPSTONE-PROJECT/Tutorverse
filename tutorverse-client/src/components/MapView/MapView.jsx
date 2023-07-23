d2cf4c45c646166da7b9690cafeb71b873d3e0ad
Tanya Chisepo <tanyachisepo@meta.com>
2023-07-23 13:59:19 -0700

    Added map markers for every tutor based on current user's location

--- tutorverse-client/src/components/MapView/MapView.jsx
+++ tutorverse-client/src/components/MapView/MapView.jsx
@@ -1,21 +1,53 @@
-import GoogleMapReact from "google-map-react";
+import { GoogleMap, useJsApiLoader , Marker} from '@react-google-maps/api';
+import { useContext, useState , useCallback} from "react";
+import { UserContext } from "../../../../userContext";
 
-export default function MapView() {
-  const defaultProps = {
-    center: {
-      lat: 37.48010072,
-      lng: -122.1674036,
-    },
-    zoom: 15,
-  };
+export default function MapView( {tutors} ) {
+    const { user } = useContext(UserContext);
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyCIpZqrYTmqR0pMG9vHXeEx_YK7QIdsXO8"
+      })
+    
+      const [map, setMap] = useState(null)
 
-  return (
-    <div style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyCIpZqrYTmqR0pMG9vHXeEx_YK7QIdsXO8" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      ></GoogleMapReact>
-    </div>
-  );
+      const containerStyle = {
+        width: '100%',
+        height: '100vh'
+      };
+      const center = {
+                lat: user.latitude,
+                lng: user.longitude
+              }
+      const onLoad = useCallback(function callback(map) {
+       
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+    
+        setMap(map)
+      }, [map])
+    
+      const onUnmount = useCallback(function callback(map) {
+        setMap(null)
+      }, [])
+    
+      return isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            { tutors.map((tutor) => (
+                <Marker 
+                    key= {tutor.id}
+                    position= {{lat: tutor.latitude,
+                    lng:tutor.longitude}}
+                />
+
+            ))}
+            
+          </GoogleMap>
+      ) : <></>
 }
