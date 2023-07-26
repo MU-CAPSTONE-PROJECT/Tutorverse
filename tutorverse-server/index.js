8b942111192a1fb598387223fb07b04c39a94272
Tanya Chisepo <tanyachisepo@meta.com>
2023-07-26 15:24:45 -0700

    Server-side code for sending and receiving messages

--- tutorverse-server/index.js
+++ tutorverse-server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 4000;
 const http = require('http').Server(app);
 const cors = require('cors');
+const { User, sequelize } = require('./data.js');
 
 const socketIO = require('socket.io')(http, {
     cors: {
@@ -12,8 +13,40 @@ const socketIO = require('socket.io')(http, {
 
 app.use(cors());
 
+const socketUser = {};
+
 socketIO.on('connection', (socket) => {
+    socket.join(socket.id)
+
     console.log(`⚡: ${socket.id} user just connected!`);
+    console.log(socket.handshake.auth)
+    const userId = socket.handshake.auth.userId
+    socket.userId = userId
+
+    if (userId){
+
+      if (socketUser[userId]==null){
+        socketUser[userId] = new Set()
+      }
+      socketUser[userId].add(socket.id)  
+    }
+    //Receiving a message
+    socket.on("receiving message", ({content, to}) => {
+      const socketIDs = socketUser[to]
+      console.log("receiving message", content,to)
+      console.log(socketIDs)
+
+      if (socketIDs){
+        socketIDs.forEach((socketId)=>{
+
+          console.log("Sending message to", socketId)
+          socket.to(socketId).emit("private message", {
+          content,
+          from: userId,
+       })
+        })
+      }
+      })
     socket.on('disconnect', () => {
       console.log('🔥: A user disconnected');
     });
