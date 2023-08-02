a3c5718358a1fa851c18242f560c02c3b9795d5f
Talia04 <tanyachisepo04@gmail.com>
2023-08-02 11:50:52 -0700

    Revert "[Updated] Live Location"

--- tutorverse-client/src/components/Dashboard/Dashboard.jsx
+++ tutorverse-client/src/components/Dashboard/Dashboard.jsx
@@ -1,58 +1,12 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import { UserContext } from "../../../../userContext";
-import axios from "axios";
 import StudentHome from "../StudentHome/StudentHome";
 import TutorHome from "../TutorHome/TutorHome";
 import "./Dashboard.css";
 
 export default function Dashboard() {
   const { user } = useContext(UserContext);
-  const [location, setLocation] = useState(null);
   console.log(user.firstName);
-
-  //Geolocation
-  useEffect(()=> {
-  if (navigator.geolocation || location===null){
-      const fetchLocation = () =>{
-          navigator.geolocation.watchPosition(success, error);
-      }; 
-      fetchLocation();
-      
-  } else {
-      console.log("No Geolocation");
-  }
-  },[location])
-  console.log(location);
-
-  function success (position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    setLocation({latitude, longitude});
-
-    //POST request to save location data to database
-    if (!(location===null)){
-      
-      try{
-        const saveLocation = async ()=>{
-
-        const response = await axios.post("http://localhost:3000/save/location",
-            {
-                location
-            }, {
-                withCredentials:true
-        })
-          console.log(response.data.message)
-        } 
-        saveLocation();
-      } catch (error){
-            console.log(error);
-        }
-    }
-  }
-
-  function error(){
-    console.log("Failed to retrieve location data")
-  }
 
   if (user.userRole === "tutor") {
     return (
