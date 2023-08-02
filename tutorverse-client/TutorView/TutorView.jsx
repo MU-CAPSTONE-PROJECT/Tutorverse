aa6019d3d68d7eec2c25d51f42217fe80185b197
Tanya Chisepo <tanyachisepo@meta.com>
2023-08-01 18:30:29 -0700

    Student can save new or update tutor rating in UI

--- tutorverse-client/TutorView/TutorView.jsx
+++ tutorverse-client/TutorView/TutorView.jsx
@@ -1,7 +1,13 @@
 import { useParams , useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { UserContext } from "../../userContext";
+import SchoolIcon from '@mui/icons-material/School';
 import Button from '@mui/material/Button';
 import Avatar from "@mui/material/Avatar";
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 import axios from "axios";
 import { useEffect } from "react";
 
@@ -10,8 +16,25 @@ import "./TutorView.css";
 export default function TutorView() {
   const [tutor, setTutor] = useState({});
   const { tutorId } = useParams();
+  const [isLoading, setIsLoading] =useState(true)
+  const { user } = useContext(UserContext);
   const navigate = useNavigate();
+  const [value,setValue] = useState(0)
+  const [rating, setRating] = useState(0)
 
+  const { firstName, lastName, major } = tutor;
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleRatingSubmit = () => {
+    handleClose();
+    setRating(value)
+    setValue(value)
+  }
+
+  console.log(tutor.rating)
   useEffect(() => {
     const fetchTutor = async () => {
       try {
@@ -19,62 +42,131 @@ export default function TutorView() {
           `http://localhost:3000/tutor/${tutorId}`,
         );
         setTutor(response.data.data);
-        console.log(tutor);
+        setIsLoading(false)
+        
+        setValue(tutor.rating)
       } catch (error) {
         console.log(error);
       }
     };
 
     fetchTutor();
-  });
+  },[ rating]);
 
-  const { firstName, lastName, major } = tutor;
-
-
+  useEffect(() => {
+    const saveRating = async  () => {
+      try {
+        const studentId = user.id
+        if(!(rating===null) && rating>0){
+          const response = await axios.post(
+          "http://localhost:3000/ratings",
+          {
+            tutorId,
+            rating: value,
+            studentId
+          }
+        )
+        console.log(response)
+        }
+      } catch (error){
+        console.log(error)
+      }
+    };
+    saveRating();
+  }, [rating])
+  
   const handleMessageBtn =()=>{
     navigate(`/chat/${tutorId}`);
   }
 
-  return (
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  if(isLoading){
+    return (
+    <div>
+    Loading...
+    </div>)
+  } else{
+    return (
     <div className="tutor-view">
-      <div className="nav-bar">
-        <div className="left">
-          <div className="logo">
-            <img src="./src/assets/educate-svgrepo-com.svg" alt="" />
-          </div>
-          <div>Tutorverse</div>
+    <div className="nav-bar">
+      <div className="left">
+        <div className="logo">
+         <SchoolIcon/>
         </div>
-        <div className="right">
-          <div>Login</div>
-          <div>Menu Hamburger</div>
-        </div>
+        <div>Tutorverse</div>
       </div>
-
-      <div className="tutor-details">
-        <div>
-          <div className="top">
-            <div className="profile-avatar">
-              <Avatar
-                alt="Profile Image"
-                src="tutorverse-client/src/assets/profile.jpg"
-                sx={{ width: 100, height: 100 }}
-              />
-            </div>
-            <div className="title">
-              <div className="name-label">
-                {firstName} {lastName}
-              </div>
-              <div className="major-label">{major}</div>
-
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="message-btn">
-        <Button onClick={handleMessageBtn}>
-          Message
-        </Button>
+      <div className="right">
+        <div>Login</div>
+        <div>Menu Hamburger</div>
       </div>
     </div>
-  );
+
+    <div className="tutor-details">
+      <div>
+        <div className="top">
+          <div className="profile-avatar">
+            <Avatar
+              alt="Profile Image"
+              src="tutorverse-client/src/assets/pexels-dziana-hasanbekava-7275385 (1).jpg"
+              sx={{ width: 100, height: 100 }}
+            />
+          </div>
+          <div className="title">
+            <div className="name-label">
+              <h2>
+                {firstName} {lastName}
+              </h2>
+            </div>
+            <div className="major-label">{major}</div>
+
+          </div>
+          <div className="rating">
+            <Box sx={{ '& > legend': { mt: 2 },}}>
+              <Typography component="legend">{tutor.rating}</Typography>
+              <Rating name="read-only" value={tutor.rating} precision={0.5} readOnly />
+            </Box>
+          </div>
+        </div>
+        <div className="rate-modal">
+          <Button onClick={handleOpen}>Rate</Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+            <Typography component="legend">Rating</Typography>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+              <Button className="submit-rating" onClick={handleRatingSubmit}>Submit</Button>
+            </Box>
+          </Modal>
+        </div>
+      </div>
+    </div>
+    <div className="message-btn">
+      <Button onClick={handleMessageBtn}>
+        Message
+      </Button>
+    </div>
+  </div>
+  )}
+    
 }
